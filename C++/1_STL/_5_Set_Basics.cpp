4df15e964d52c3c55f0153d3756a38198ae3c98a
ritesh swami <riteshswamiofficial@gmail.com>
2024-11-18 13:02:16 +0530

    18-11-24

--- C++/1_STL/_5_Set_Basics.cpp
+++ C++/1_STL/_5_Set_Basics.cpp
@@ -1,9 +1,25 @@
 // It is just like the map but here values are not stored , it just a collection of keys which are stored in a sorted order and obviously its elements(keys) are unique .
-
+// also be defined as it is a collection of unique numbers or elements .
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    
+    set<string> st;
+    st.insert("abc");
+    st.insert("sff");
+    st.insert("zvc");
+
+    auto it = st.find("abc");
+
+    // for(string &value : st)
+    // {
+    //     cout<<st<<endl;
+    // }
+    for(auto it = st.begin() ; it != st.end() ; it++)
+    {
+        cout<<(*it)<<endl;
+    }
+
+
 }
