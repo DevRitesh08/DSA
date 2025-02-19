333edaa44990b9c6e222ff04ab5b1053f716bf2c
ritesh swami <riteshswamiofficial@gmail.com>
2025-02-19 20:34:15 +0530

    Refactor Next Permutation implementation: enhance comments and optimize code structure

--- ARRAYS/Easy/Leaders_in_an_Array_problem.cpp
+++ ARRAYS/Easy/Leaders_in_an_Array_problem.cpp
@@ -5,29 +5,29 @@ using namespace std;
 
 // Brute force approach to find leaders in an array
 
-// int main() 
-// {
-//     int arr[] = {10, 22, 6, 12, 7, 9, 5, 2};  // Leaders are 22, 12, 9, 5, 2
-//     int n = sizeof(arr) / sizeof(arr[0]);
+int main() 
+{
+    int arr[] = {10, 22, 6, 12, 7, 9, 5, 2};  // Leaders are 22, 12, 9, 5, 2
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-//     for (int i = 0; i < n; i++) 
-//     {
-//         bool leader = true;  // Assume arr[i] is a leader
+    for (int i = 0; i < n; i++) 
+    {
+        bool leader = true;  // Assume arr[i] is a leader
         
-//         for (int j = i + 1; j < n; j++) 
-//         {
-//             if (arr[i] < arr[j]) // If any element on the right is greater, it's not a leader
-//             {  
-//                 leader = false;
-//                 break;
-//             }
-//         }
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (arr[i] < arr[j]) // If any element on the right is greater, it's not a leader
+            {  
+                leader = false;
+                break;
+            }
+        }
 
-//         if (leader)  // If it's still true, print the leader
-//             cout << arr[i] << " ";
-//     }
-//     return 0;
-// }
+        if (leader)  // If it's still true, print the leader
+            cout << arr[i] << " ";
+    }
+    return 0;
+}
 
 // Time complexity is O(N^2)
 // Space complexity is O(1)
@@ -36,34 +36,34 @@ using namespace std;
 
 // Optimal approach (Reverse traversal approach)
 
-#include <bits/stdc++.h> 
-using namespace std;
+// #include <bits/stdc++.h> 
+// using namespace std;
 
-int main() {
-    int arr[] = {16, 17, 4, 3, 5, 2};
-    int n = sizeof(arr) / sizeof(arr[0]);  
+// int main() {
+//     int arr[] = {16, 17, 4, 3, 5, 2};
+//     int n = sizeof(arr) / sizeof(arr[0]);  
 
-    vector<int> ans;  
-    int max_element = INT_MIN;  
+//     vector<int> ans;  
+//     int max_element = INT_MIN;  
 
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] > max_element) {
-            ans.push_back(arr[i]);
-        }
-        max_element = max(max_element, arr[i]);
-    }
+//     for (int i = n - 1; i >= 0; i--) {
+//         if (arr[i] > max_element) {
+//             ans.push_back(arr[i]);
+//         }
+//         max_element = max(max_element, arr[i]);
+//     }
 
-    // Reverse to maintain order as found from right to left
-    sort(ans.begin(), ans.end());  // time complexity is O(N * logN)
+//     // Reverse to maintain order as found from right to left
+//     sort(ans.begin(), ans.end());  // time complexity is O(N * logN)
 
-    cout << "The leader elements are: [";
-    for (int i = 0; i < ans.size(); i++) {  
-        cout << ans[i] << " ";
-    }
-    cout << "]" << endl;  
+//     cout << "The leader elements are: [";
+//     for (int i = 0; i < ans.size(); i++) {  
+//         cout << ans[i] << " ";
+//     }
+//     cout << "]" << endl;  
 
-    return 0;  
-}
+//     return 0;  
+// }
 
 //if sort function is not used then time coplexity will be O(N).
 // Time complexity is O(N + N*logN) that boils down to O(N*logN) as in In big-O notation, we keep only the dominant term (the term that grows the fastest as n increases) , O(n log n) grows faster than O(n) for large n, so we drop the lower-order terms.
--- ARRAYS/Medium/Next_Permutaion.cpp
+++ ARRAYS/Medium/Next_Permutaion.cpp
@@ -1,37 +1,62 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 
 
 // Brute solution 
 
+// Approach :
 
+// Step 1: Find all possible permutations of elements present and store them.
+// Step 2: Search input from all possible permutations.
+// Step 3: Print the next permutation present right after it.
 
+// Time complexity will be O(N!*N) , N represents the number of elements presents in input array , also for searching input arrays from all possible permutations will take N! . therefore , it has a very high time complexity of O(N!*N) , hence only tell the approach , but if asked to write algo  then refer to lecture 11 and 12 of recursion .
 
 
 
 
 // Better Solution
 
-int main() 
-{
-    int arr[] = {1,3,2};
+// C++ provides an in-built function called next_permutation() which directly returns the lexicographically next greater permutation of the input.
+
+// int main() 
+// {
+//     int arr[] = {1,3,2};
     
-    next_permutation(arr,arr + 3);// using in-built function of C++
+//     next_permutation(arr,arr + 3);// using in-built function of C++
     
-    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
+//     cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
     
-    return 0;
-}
+//     return 0;
+// }
+
+// Time Complexity: O(N) (worst case, when the entire array needs to be reversed).
+// Space Complexity: O(1) (modifies the array in-place, no extra space used).
+
 
 
 // Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
+int main() {
+    vector<int> A = {1,3,2};
+    int n = A.size(), ind = -1;
 
-    
+    // Find the breakpoint
+    for (int i = n - 2; i >= 0; i--)
+        if (A[i] < A[i + 1]) { ind = i; break; }
+
+    if (ind == -1) { reverse(A.begin(), A.end()); }  
+    else {
+        for (int i = n - 1; i > ind; i--)
+            if (A[i] > A[ind]) { swap(A[i], A[ind]); break; }
+        reverse(A.begin() + ind + 1, A.end());
+    }
+
+    for (int num : A) cout << num << " ";
 }
+ 
+// time complexity is O(N) .
+// space complexity is O(1) .
