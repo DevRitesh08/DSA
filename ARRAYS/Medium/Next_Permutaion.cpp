67e571c0278c5a4032e55de2428ae03e187c7bf9
ritesh swami <riteshswamiofficial@gmail.com>
2025-02-19 20:02:40 +0530

    Add Next Permutation implementation

---
+++ ARRAYS/Medium/Next_Permutaion.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+
+// Brute solution 
+
+
+
+
+
+
+
+// Better Solution
+
+int main() 
+{
+    int arr[] = {1,3,2};
+    
+    next_permutation(arr,arr + 3);// using in-built function of C++
+    
+    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
+    
+    return 0;
+}
+
+
+// Optimal Approach
+
+int main()
+{
+
+    
+}
